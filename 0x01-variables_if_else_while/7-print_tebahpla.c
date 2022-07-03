dbdd794b91b912e586784dcd608fb83d3725acfa
chukwudozie <emkychuks062@gmail.com>
2022-07-02 17:22:09 -0700

    print chars in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print in reverse
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = 'z';
+
+	while (num > 'a')
+	{
+		putchar(num);
+		c--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
