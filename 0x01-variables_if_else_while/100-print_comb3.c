7664823c81259a85dea3898399cf69b2a3809d84
chukwudozie <emkychuks062@gmail.com>
2022-07-03 12:25:24 -0700

    print two digit conbinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all possible two digits combinations
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char first = '0';
+	char second = '1';
+
+	while (first <= '9')
+	{
+		putchar(first);
+		putchar(second);
+		putchar(', ');
+		first++;
+	}
+
+	return (0);
+}
