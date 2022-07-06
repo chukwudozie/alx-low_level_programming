6264d75a8d6b60a967d94d6f8e1f38ec03fbf215
chukwudozie <emkychuks062@gmail.com>
2022-07-06 16:23:17 -0700

    print factors of 3 and 5 less thank 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,26 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
+ * main - entry point
+ * Return: result
+ */
+
+int main(void)
+{
+	int c = 0;
+	int result = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			result += c;
+		}
+		c++;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
