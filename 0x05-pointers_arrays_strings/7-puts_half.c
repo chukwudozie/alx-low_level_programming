b4f5e066612dfeea16380efb6c57232354e5e3ac
chukwudozie <emkychuks062@gmail.com>
2022-07-12 23:42:45 -0700

    put half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string followed by new line
+ * @s: input string
+ * Return: void
+ */
+
+void puts_half(char *s)
+{
+	int length = 0;
+	int counter = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (counter <= length / 2)
+	{
+		_putchar(s[counter]);
+		counter++;
+	}
+
+	_putchar('\n');
+}
