c3199d09b7f1d903cff2a4c5577eae4895b9e05f
chukwudozie <emkychuks062@gmail.com>
2022-07-12 17:07:44 -0700

    revert string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses an input string
+ * @s: input string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int i = 0;
+	char temp;
+
+	while (s[length] != '\0')
+		length++;
+
+	while (i < length++)
+	{
+		temp = s[i];
+		s[i++] = s[length];
+		s[length] = temp;
+	}
+}
