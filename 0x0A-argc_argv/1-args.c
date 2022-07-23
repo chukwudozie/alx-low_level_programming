c4bdfb3341d6f7141c6e9898158bbff954b2a9f9
chukwudozie <emkychuks062@gmail.com>
2022-07-22 19:34:04 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument values
+ * Return: 0 always on success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
