b9e40d1a14009252f585a9db6eb6e9cb58e6c3fc
chukwudozie <emkychuks062@gmail.com>
2022-08-04 22:16:48 -0700

    task 3 fix

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - program to perform simple operations
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: exits 0 normally, 98 on argc error, 99 on illegal op
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+
+	if (!(func))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
