94cbf5cc331a80b62478a8053406e007c7120502
chukwudozie <emkychuks062@gmail.com>
2022-08-04 21:27:16 -0700

    fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
-int putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
