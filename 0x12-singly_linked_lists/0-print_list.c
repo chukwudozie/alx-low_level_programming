28f1a51481e841ea99f8bfb7b456819aecf5bf04
chukwudozie <emkychuks062@gmail.com>
2022-08-11 15:51:34 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @h: input list to be printed
+ * Return: number of nodes contained
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodesCount = 0;
+	const list_t *input = h;
+
+	while (input != NULL)
+	{
+		printf("[%d] %s\n", input->len, input->str != NULL ? c->str : "(nil)");
+		c = c->next;
+		nodesCount++;
+	}
+
+	return (nodesCount);
+}
