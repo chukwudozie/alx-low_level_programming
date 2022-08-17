3cdec4127fe518234c3ec6c0159c20a193e783a7
chukwudozie <emkychuks062@gmail.com>
2022-08-16 21:35:34 -0700

    advanced 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - function to reverse a linked list
+ * @head: pointer to head
+ * Return: pointer to the neew head
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous = NULL;
+	listint_t *nxt = NULL;
+
+	while (*head == NULL)
+	{
+		nxt = (*head)->next;
+		(*head)->next = previous;
+		previous = *head;
+		*head = nxt;
+	}
+	*head = previous;
+
+	return (*head);
+}
