200941d764c57706868732d9fc4d96570c7ff0f3
chukwudozie <emkychuks062@gmail.com>
2022-08-16 20:29:09 -0700

    fix task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_nodeint_at_index - function to return the nth node of a list
  * @head: pointer to list node head
- * index: index of the node starting at 0
+ * @index: index of the node starting at 0
  * Return: nth node
  */
 
@@ -14,12 +14,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	while (y < index)
+	while (i < index)
 	{
 		head = head->next;
 		if (head == NULL)
 			return (NULL);
-		y++;
+		i++;
 	}
 
 	return (head);
